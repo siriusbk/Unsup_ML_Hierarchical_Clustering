a65244823bd3ceefb0ab3ea6e2e5b19c00587270
siriusbk <sirius.bakin@gmail.com>
2016-05-29 12:09:38 +0300

    _-_-_-_-_-_-_-_-_-_-_-_-_ Attention !

--- Unsup_ML_Hierarchical_Clustering/__init__.py
+++ Unsup_ML_Hierarchical_Clustering/__init__.py
@@ -4,6 +4,9 @@ from sklearn.cluster import MeanShift
 from sklearn.datasets.samples_generator import make_blobs
 from mpl_toolkits.mplot3d import Axes3D
 
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
 centers = [[1,1,1],[5,5,5],[3,10,10]]
 
 X, _ = make_blobs(n_samples=100, cluster_std=1.5)
@@ -23,12 +26,10 @@ colors = 10 * ['r', 'g', 'b', 'c', 'k', 'y', 'm']
 print(colors)
 print(labels)
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
 
 for i in range(len(X)):
 
-    ax.scatter(X[i][1], X[i][0], X[i][2], c=colors[labels[i]], marker='o')
+    ax.scatter(X[i][0], X[i][1], X[i][2], c=colors[labels[i]], marker='o')
 
 ax.scatter(cluster_centers[:, 0], cluster_centers[:, 1], cluster_centers[:, 2], marker="x", color='k', s=150, linewidths=5, zorder=10)
 
